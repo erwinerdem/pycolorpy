afb7a75cba14869f3c1fcf2dbfff7b9ddadc3010
erwinerdem <erwin.erdem@live.nl>
2021-04-24 01:13:47 +0200

    Added background color as argument

--- pycolorpy.py
+++ pycolorpy.py
@@ -1,57 +1,76 @@
-def color_options(colored_string: str, options) -> str:
-    options = [option.lower() for option in options]
-    BRIGHT = ['bright', 'bold', 'b']
-    DARK = ['dark', 'dim', 'd']
-    ITALIC = ['italic', 'i']
-    UNDERLINE = ['underline', 'u']
-    BLINK = ['blink', 'bl']
-    SELECTED = ['selected', 's']
-    all_valid_options = BRIGHT + DARK + ITALIC + UNDERLINE + BLINK + SELECTED
-    invalid_options = ', '.join(['"'+option+'"' for option in options if option not in all_valid_options])
-    if invalid_options:
-        raise Exception('Invalid option(s) used!\n'+invalid_options+' are not valid options...')
-    # TODO make this more efficient?
-    if any(option in BRIGHT for option in options):
-        colored_string = '\33[1m'+colored_string
-    if any(option in DARK for option in options):
-        colored_string = '\33[2m'+colored_string
-    if any(option in ITALIC for option in options):
-        colored_string = '\33[3m'+colored_string
-    if any(option in UNDERLINE for option in options):
-        colored_string = '\33[4m'+colored_string
-    if any(option in BLINK for option in options):
-        colored_string = '\33[5m'+colored_string
-    if any(option in SELECTED for option in options):
-        colored_string = '\33[7m'+colored_string
+VALID_OPTIONS = {
+    'bright': '\33[1m',
+    'dark': '\33[2m',
+    'italic': '\33[3m',
+    'underline': '\33[4m',
+    'blink': '\33[5m',
+    'selected': '\33[7m',
+    }
+
+VALID_COLORS = {
+    'black': 30,
+    'red': 31,
+    'green': 32,
+    'yellow': 33,
+    'blue': 34,
+    'magenta': 35,
+    'cyan': 36,
+    'white': 37,
+    }
+
+def color_options(colored_string: str, options):
+    for option in options:
+        option = option.lower()
+        if option in VALID_OPTIONS.keys():
+            colored_string = VALID_OPTIONS[option]+colored_string
+        else:
+            raise Exception(
+                'Invalid option used.\n"'+option+
+                '" not a valid option.\nValid options are:\n'+
+                ', '.join([k for k in VALID_OPTIONS.keys()])
+                )
     return colored_string
 
-def colored_string(string: str, color_number: int, options, background) -> str:
+def color_bg(colored_string, background):
+    if background in VALID_COLORS.keys():
+        return '\33['+str(VALID_COLORS[background]+10)+'m'+colored_string
+    raise Exception(
+                'Invalid color used.\n"'+background+
+                '" not a valid color.\nValid colors are:\n'+
+                ', '.join([k for k in VALID_COLORS.keys()])
+                )
+
+def colored_string(string: str, color_number: int, options, background):
     colored_string = '\33['+str(color_number)+'m'+string+'\33[0m'
-    if background:
-        return '\33['+str(color_number+10)+'m'+string+'\33[0m'
-    colored_string = color_options(colored_string, options)
-    return colored_string
+    if options is None and background is None:
+        return colored_string
+    elif options is not None and background is None:
+        return color_options(colored_string, options)
+    elif options is None and background is not None:
+        return color_bg(colored_string, background)
+    elif options is not None and background is not None:
+        return color_bg(color_options(colored_string, options), background)
 
-def black(string: str, options: list[str] = [], background: str = '') -> str:
+def black(string: str, options = None, background = None) -> str:
     return colored_string(string, 30, options, background)
 
-def red(string: str, options: list[str] = [], background: bool = False) -> str:
+def red(string: str, options = None, background = None) -> str:
     return colored_string(string, 31, options, background)
 
-def green(string: str, options: list[str] = [], background: bool = False) -> str:
+def green(string: str, options = None, background = None) -> str:
     return colored_string(string, 32, options, background)
 
-def yellow(string: str, options: list[str] = [], background: bool = False) -> str:
+def yellow(string: str, options = None, background = None) -> str:
     return colored_string(string, 33, options, background)
 
-def blue(string: str, options: list[str] = [], background: bool = False) -> str:
+def blue(string: str, options = None, background = None) -> str:
     return colored_string(string, 34, options, background)
 
-def magenta(string: str, options: list[str] = [], background: bool = False) -> str:
+def magenta(string: str, options = None, background = None) -> str:
     return colored_string(string, 35, options, background)
 
-def cyan(string: str, options: list[str] = [], background: bool = False) -> str:
+def cyan(string: str, options = None, background = None) -> str:
     return colored_string(string, 36, options, background)
 
-def white(string: str, options: list[str] = [], background: bool = False) -> str:
+def white(string: str, options = None, background = None) -> str:
     return colored_string(string, 37, options, background)
